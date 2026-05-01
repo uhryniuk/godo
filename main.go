976f46c71f85cb82323fa3e0f6c6297adea696e0
uhryniuk <duhry99@gmail.com>
2026-05-01 15:09:15 -0500

    chore: remove legacy log writing
    Signed-off-by: uhryniuk <duhry99@gmail.com>

--- main.go
+++ main.go
@@ -1,25 +1,7 @@
 package main
 
-// import "fmt"
-
-import (
-	"github.com/uhryniuk/godo/cmd"
-	"log/slog"
-	"os"
-	// "github.com/spf13/cobra"
-)
+import "github.com/uhryniuk/godo/cmd"
 
 func main() {
-	logFile, err := os.OpenFile("godo.jsonl", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-
-	if err != nil {
-		panic(err)
-	}
-	defer logFile.Close()
-
-	jsonFileHandler := slog.NewJSONHandler(logFile, nil)
-
-	logger := slog.New(jsonFileHandler)
-	logger.Info("User logged in", slog.String("user", "alice"))
 	cmd.Execute()
 }
